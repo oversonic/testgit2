1fd66e767e51d8393337c9056c3456132268f138
Jirasak <Jirasak@YB09892278>
2017-06-19 16:27:06 +0700

    c1

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php 
+echo "hellow world";
+if (isset($_POST["cmd"]) && $_POST["cmd"] == "getOrgInfo") {
+	echo "200&2000/02/01 07:00:00&3&3072&3&121.11";
+    
+} else {
+    echo "200";
+    
+}
+?>
