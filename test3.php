0c344151a03b0be4e7d77608d7b940f003bce215
Jirasak <Jirasak@YB09892278>
2017-06-19 16:45:36 +0700

    c4

--- test3.php
+++ test3.php
@@ -1,3 +1,20 @@
 <?php
 
 echo "abc";
+?>
+
+
+==[IDE]== Jun 19, 2017 3:33:24 PM Retrieving files...
+==[IDE]== Jun 19, 2017 3:33:24 PM Retrieving files... finished.
+==[IDE]== Jun 19, 2017 4:21:04 PM Pushing - testgit
+git branch
+git remote -v
+setting up remote: origin
+git submodule status
+git push https://github.com/oversonic/hellowgit.git refs/heads/master:refs/heads/master
+
+Remote Repository Updates
+Branch Update : master
+Old Id        : 02f54f95c570d218ef6717252941e4189995c53d
+New Id        : 76e41d3371d9a2db1e697d7d90a2da2f1dd36b81
+Result        : REJECTED_NONFASTFORWARD
